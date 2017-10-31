e471c279d99a28060ba22f750c1e6734832da4da
jasminetpm <jasmine.tpm@u.yale-nus.edu.sg>
2017-10-31 14:07:11 +0800

    Added Javadoc

--- src/model/Triangle.java
+++ src/model/Triangle.java
@@ -5,7 +5,16 @@ import java.awt.Point;
 public class Triangle {
 	
 	private Point[] points;
-	
+
+	/**
+	 * Constructor for a triangle
+	 * @param x1 the X coordinate of the first point
+	 * @param y1 the Y coordinate of the first point
+	 * @param x2 the X coordinate of the second point
+	 * @param y2 the Y coordinate of the second point
+	 * @param x3 the X coordinate of the third point
+	 * @param y3 the Y coordinate of the third point
+	 */
 	public Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
 	{
 		this.points = new Point[3];
